4babcadef64dbcae48cecb29eb499822ca45bd58
Thomas Gallant <60023923+Thomas-Gallant@users.noreply.github.com>
2024-09-08 19:45:29 -0400

    Adding point boxes, with the boxes disappearing upon contact with ball (#2)
    * Adding point blocks
    
    * Blocks are now being deleted on impact
    
    * Refactoring and keeping track of how many boxes are left
    
    * cleanup of print statements
    
    * When game is over go to home page

--- main/blocks/yellow-block.go
+++ main/blocks/yellow-block.go
@@ -1,46 +1,33 @@
+components {
+  id: "yellow-block"
+  component: "/main/blocks/yellow-block.script"
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/main/blocks/yellow-block.atlas\"\n"
-  "default_animation: \"yellow-box\"\n"
+  data: "default_animation: \"yellow-box\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/blocks/yellow-block.atlas\"\n"
+  "}\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
   "mass: 0.0\n"
-  "friction: 0.1\n"
-  "restitution: 0.5\n"
+  "friction: 0.0\n"
+  "restitution: 1.0\n"
   "group: \"default\"\n"
   "mask: \"default\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
   "    count: 3\n"
@@ -49,20 +36,5 @@ embedded_components {
   "  data: 332.5\n"
   "  data: 10.0\n"
   "}\n"
-  "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
-  "bullet: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
---
+++ main/boxes/boxes.go
@@ -0,0 +1,8 @@
+components {
+  id: "boxes"
+  component: "/main/boxes/boxes.factory"
+}
+components {
+  id: "boxes-script"
+  component: "/main/boxes/boxes.script"
+}
