d03e56d2fda2ede69980db093da78788b1f1d410
Thomas Gallant <60023923+Thomas-Gallant@users.noreply.github.com>
2024-09-08 17:38:46 -0400

    Foundations and Menu (#1)
    * reseting location, rotation, and linear velocity of bouncer
    
    * Adding local variables back
    
    * getting to a point where things are technically playable
    
    * adjusting area bouncer can go
    
    * All walls are working, switching from static to dynamic
    
    * updating spacing to not see walls
    
    * bouncer now always stays within window size
    
    * updating readme with todos and credits
    
    * using apply_force instead of linear_velocity to fix the decelleration issue
    
    * decreasing bouncer speed
    
    * adding check boxes on completed items
    
    * updating ball speed and cleanup
    
    * making the walls static
    
    * Adding main menu
    
    * Adding druid library
    
    * adding buttons assets
    
    * Adding quit button to game
    
    * Adding license
    
    * updating list checkbox
    
    * moving ball apply_force to init function from update function
    
    * updating documentation
    
    * Updating defold setup docs
    
    * updating todo items
    
    * removing dynamic positioning of ball and bouncer
    
    * adding manifest files to git ignore
    
    * adding bunle folder to git ignore
    
    * Fixing the flying bug
    
    * changing back to kinematic object for bouncer
    
    * updating ball script speed
    
    * adding back collisions with walls for bouncer
    
    * using dt for the magic solution
    
    * removing the dt
    
    * adding comment to code
    
    * Updating the font

--- README.md
+++ README.md
@@ -12,6 +12,27 @@ Happy Defolding!
 
 ---
 
-# Setup
+# Setup with VSCode
 
-Run the `Defold Buddy: Initialize` command to set up the workspace.
+Run the `Defold Buddy: Initialize` command in vscode command palette to set up the workspace.
+Run the `Defold Buddy: Fetch Libraries` command in vscode command palette to fetch libraries
+
+# Setup with Defold
+
+Fetch libraries `Project --> Fetch Libraries` in menu bar
+
+## TODO Items
+- [x] Fix issue with ball moving slow
+- [x] Fix issue with ball speed changing on window resize
+- [ ] Fix issue with ball speed changing on window move
+- [ ] Use factory to create point blocks, make them Kinematic
+- [ ] Point blocks disappear upon contact
+- [ ] Add point system
+- [ ] Add timer
+- [x] Add menu
+- [x] Add back falling to bouncer
+- [ ] Create deployment pipeline to Itch.io
+
+## Credits
+- Assets made with [Microsoft PowerPoint](https://www.microsoft.com/en-us/microsoft-365/powerpoint)
+- [druid component library](https://github.com/Insality/druid)
--- main/walls/vertical-left-wall.go
+++ main/walls/vertical-left-wall.go
@@ -1,6 +1,39 @@
-components {
-  id: "bouncer-script"
-  component: "/main/bouncer/bouncer.script"
+embedded_components {
+  id: "vertical-left-wall"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_STATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.0\n"
+  "restitution: 1.0\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 320.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 0.5\n"
+  "  data: 320.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
   position {
     x: 0.0
     y: 0.0
@@ -20,58 +53,17 @@ embedded_components {
   "default_animation: \"bouncer\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "bouncer"
-  type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
-  "mass: 0.0\n"
-  "friction: 0.0\n"
-  "restitution: 1.0\n"
-  "group: \"default\"\n"
-  "mask: \"default\"\n"
-  "embedded_collision_shape {\n"
-  "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
-  "    position {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "    }\n"
-  "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
-  "    }\n"
-  "    index: 0\n"
-  "    count: 3\n"
-  "  }\n"
-  "  data: 992.5\n"
-  "  data: 85.0\n"
-  "  data: 10.0\n"
+  "size {\n"
+  "  x: 1.0\n"
+  "  y: 640.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
   "}\n"
-  "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
-  "bullet: false\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
   ""
   position {
     x: 0.0
-    y: 0.0
+    y: 320.0
     z: 0.0
   }
   rotation {
